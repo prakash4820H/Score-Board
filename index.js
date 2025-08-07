855e314192e1e650e12b164d674b763d98856e5c
P. Jaya Prakash <84934335+prakash4820H@users.noreply.github.com>
2025-08-07 09:04:10 +0530

    add stuff

---
+++ index.js
@@ -0,0 +1,28 @@
+let homeS = document.getElementById("home-score")
+let h = 0
+function add1(){
+    h +=1
+    homeS.textContent = h
+}
+function add2(){
+    h +=2
+    homeS.textContent = h
+}
+function add3(){
+    h +=3
+    homeS.textContent = h
+}
+let guestS = document.getElementById("guest-score")
+let g = 0
+function Add1(){
+    g +=1
+    guestS.textContent = g
+}
+function Add2(){
+    g +=2
+    guestS.textContent = g
+}
+function Add3(){
+    g +=3
+    guestS.textContent = g
+}
